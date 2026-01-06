faf20c30d2eb8998785053e9c82f5b96b85bf5f7
Vano <Van.Poghosyan@gmail.com>
2026-01-06 23:37:03 +0400

    Initial commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 import { OrderProvider } from './context/OrderContext.jsx'
@@ -10,9 +10,9 @@ createRoot(document.getElementById('root')).render(
     <StrictMode>
       <LanguageProvider>
         <OrderProvider>
-          <BrowserRouter>
+          <HashRouter>
             <App />
-          </BrowserRouter>
+          </HashRouter>
         </OrderProvider>
       </LanguageProvider>
     </StrictMode>
