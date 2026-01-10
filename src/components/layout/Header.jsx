a44d9faef52a9deff1ce372cf8275124b3916a60
Vano <Van.Poghosyan@gmail.com>
2026-01-10 14:49:31 +0400

    fix site.webmanifest

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -9,9 +9,8 @@ const Header = () => {
   const isProjectPage = location.pathname.startsWith('/portfolio/')
 
   return (
-      <header className='sticky top-0 z-50 border-b border-[rgb(var(--border))] bg-[rgb(var(--bg))]/80 backdrop-blur'>
+      <header className='sticky left-0 top-0 z-50 border-b border-[rgb(var(--border))] bg-[rgb(var(--bg))]/80 backdrop-blur'>
         <div className='container flex h-16 items-center justify-between'>
-
           <Logo />
 
           { !isProjectPage && <Navigation variant='header' /> }
